ccd0c422d8be83d365d675f5b040b72a2fe57041
Crowdbotics <engineering@crowdbotics.com>
2023-10-11 09:05:44 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sparkling-poetry-d-121699",
+  url: "https://sparkling-poetry-d-121699.botics.co",
+  api: "https://sparkling-poetry-d-121699.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
